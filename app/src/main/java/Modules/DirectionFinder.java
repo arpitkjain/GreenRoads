6a13e91e3906fa88ace41853a30cb62b174a6e7c
arijitkar98 <arijit.kar14@gmail.com>
2018-03-10 20:58:02 +0530

    Added new API key

--- app/src/main/java/Modules/DirectionFinder.java
+++ app/src/main/java/Modules/DirectionFinder.java
@@ -24,7 +24,7 @@ import java.util.List;
  */
 public class DirectionFinder {
     private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?";
-    private static final String GOOGLE_API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
+    private static final String GOOGLE_API_KEY = "AIzaSyBRlTaNk61pKHLzUSPBd4oAzzCkjakx-DA";
     private DirectionFinderListener listener;
     private String origin;
     private String destination;
