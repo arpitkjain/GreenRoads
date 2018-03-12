773ef78018b4518f83e982771ed2e84e2412d2c5
arijitkar98 <arijit.kar14@gmail.com>
2018-03-12 13:36:38 +0530

    Added Roads API in ReviewActivity

--- app/src/main/java/com/itshareplus/googlemapdemo/ReviewActivity.java
+++ app/src/main/java/com/itshareplus/googlemapdemo/ReviewActivity.java
@@ -2,6 +2,7 @@ package com.itshareplus.googlemapdemo;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -20,6 +21,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,12 +30,20 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.MapFragment;
 import android.widget.Toast;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.view.View.OnClickListener;
+
+import Modules.NearestRoad;
 
 public class ReviewActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private List<Marker> originMarkers = new ArrayList<>();
     private LatLng point;
+    private RatingBar ratingBar;
+    private float ratingValue;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +53,44 @@ public class ReviewActivity extends FragmentActivity implements OnMapReadyCallba
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.review_map);
         mapFragment.getMapAsync(this);
+        addListenerOnRatingBar();
+        addListenerOnButton();
+    }
+    public void addListenerOnRatingBar() {
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+        //if rating value is changed,
+        //display the current rating value in the result (textview) automatically
+        ratingBar.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                ratingValue = rating;
+            }
+        });
+    }
+
+    public void addListenerOnButton() {
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        //if click on me, then display the current rating value.
+        btnSubmit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ReviewActivity.this, "Rating Registered: " + Float.toString(ratingValue), Toast.LENGTH_SHORT).show();
+                try {
+                    NearestRoad nearestRoad = new NearestRoad(point, ratingValue);
+                    nearestRoad.execute();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(ReviewActivity.this, MapsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+//                finishActivity(ReviewActivity.this);
+            }
+        });
     }
 
     @Override
@@ -66,26 +114,28 @@ public class ReviewActivity extends FragmentActivity implements OnMapReadyCallba
 //        }
         mMap.setOnMapLongClickListener(new OnMapLongClickListener() {
             @Override
-            public void onMapLongClick(LatLng point) {
-                Toast.makeText(ReviewActivity.this,point.latitude+" : "+point.longitude,Toast.LENGTH_SHORT).show();
+            public void onMapLongClick(LatLng newPoint) {
+                // Toast.makeText(ReviewActivity.this,point.latitude+" : "+point.longitude,Toast.LENGTH_SHORT).show();
                 // Creating a marker
                 MarkerOptions markerOptions = new MarkerOptions();
 
                 // Setting the position for the marker
-                markerOptions.position(point);
+                markerOptions.position(newPoint);
 
                 // Setting the title for the marker.
                 // This will be displayed on taping the marker
-                markerOptions.title(point.latitude + " : " + point.longitude);
+                markerOptions.title(newPoint.latitude + " : " + newPoint.longitude);
 
                 // Clears the previously touched position
                 mMap.clear();
 
                 // Animating to the touched position
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(point));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(newPoint));
 
                 // Placing a marker on the touched position
                 mMap.addMarker(markerOptions);
+
+                point = newPoint;
             }
         });
     }
