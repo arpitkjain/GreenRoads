cf2447ecc2f4332db5256a27cba1dcb1ae0ab4e5
Eeshan Gupta <eeshan9815@gmail.com>
2018-03-25 08:36:17 +0530

    add location support

--- app/src/main/java/com/itshareplus/googlemapdemo/SafeActivity.java
+++ app/src/main/java/com/itshareplus/googlemapdemo/SafeActivity.java
@@ -273,6 +273,16 @@ public class SafeActivity extends FragmentActivity implements OnMapReadyCallback
 //            return;
 //
 //        }
+        if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            mMap.setMyLocationEnabled(true);
+        }
         mMap.setOnMapLongClickListener(new OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng newPoint) {
